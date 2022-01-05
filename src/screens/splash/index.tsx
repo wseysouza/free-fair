01bffcd9f0829876bd6747bee557a64e2a6b9d18
Wesley de Souza <wseysouza@gmail.com>
2022-01-05 15:12:23 -0300

    create screen splash

--- src/screens/splash/index.tsx
+++ src/screens/splash/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Image } from 'react-native';
 
-import background from "../../assets/logo.png";
+// import background from "../../assets/logo.png";
 
 // import {} from './styles';
 
-export function Splash() {
-    return (
-        <Image source={background} />
-    )
-}
+// export function Splash() {
+//     return (
+//         <Image source={background} />
+//     )
+// }
