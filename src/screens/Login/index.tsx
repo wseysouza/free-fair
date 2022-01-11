3e3fa36230279112eb0c309f19e272748071d6b0
Wesley de Souza <wseysouza@gmail.com>
2022-01-11 16:31:56 -0300

    details screen login

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,16 +1,35 @@
 import React from "react";
+import { TextInput, Text, Image } from 'react-native';
+import { MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons';
 
 import background from "../../assets/background.png";
-import { Container, Background, Content } from "./styles";
-import { Input } from "../../components/Form/Input";
+import google from "../../assets/google.png"
+
+import { Container, Background, Content, InputRow, ButtonsRow, ButtonGoogle, ButtonFacebook } from "./styles";
+
 
 export function Login() {
     return (
         <Container>
             <Background source={background} />
             <Content>
-                <Input placeholder="Phone Number" />
+                <InputRow>
+                    <MaterialCommunityIcons name="phone" />
+                    <TextInput placeholder="Phone Number" />
+                </InputRow >
+
+                <ButtonsRow>
+                    <ButtonGoogle>
+                        <Image source={google} />
+                        <Text>Google</Text>
+                    </ButtonGoogle>
+
+                    <ButtonFacebook>
+                        <FontAwesome name="facebook" />
+                        <Text>Facebook</Text>
+                    </ButtonFacebook>
+                </ButtonsRow>
             </Content>
-        </Container>
+        </Container >
     )
 }
--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -14,11 +14,40 @@ export const Background = styled.Image`
 `;
 
 export const Content = styled.View`
+    padding: 30px;
     width: 100%;
     height: 201px;
+    justify-content: space-between;
+    border-Top-Left-Radius: 20px;
+    border-Top-Right-Radius: 20px;
+
 
     position: absolute;
 
-    background: red;
+    background: ${({ theme }) => theme.colors.text};
 
 `;
+
+export const InputRow = styled.View`
+    width: 100%;
+    height: 60px;
+
+    flex-direction: row;
+    align-items: center;
+
+
+    border-radius: 6px ;
+    background-color: ${({ theme }) => theme.colors.territory};
+`;
+
+export const ButtonsRow = styled.View`
+    width: 100%;
+    height: 60px;
+
+    background-color: ${({ theme }) => theme.colors.territory};
+
+`;
+
+export const ButtonGoogle = styled.TouchableOpacity``;
+
+export const ButtonFacebook = styled.TouchableOpacity``;
